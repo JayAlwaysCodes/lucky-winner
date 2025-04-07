ad694087832ec4700eb5a2dbf5b742999350691d
jayalwayscodes <johnsonjardocs@gmail.com>
2025-04-07 19:46:53 +0100

    deploy scripts are working

--- script/deploy.py
+++ script/deploy.py
@@ -12,11 +12,16 @@ def deploy() -> VyperContract:
     print(f"Mock VRF Coordinator at: {vrf_coordinator}")
 
     # Raffle parameters (example values, adjust as needed)
-    entrance_fee = 0.01 * 10**18  # 0.01 ETH in wei
+    entrance_fee =  10**16  # 0.01 ETH in wei
     interval = 3600  # 1 hour in seconds
     gas_lane = b"\x00" * 32 # Replace with actual key hash (32 bytes)
     subscription_id = 1234  # Replace with your Chainlink subscription ID
     callback_gas_limit = 100000  # Gas limit for VRF callback
+    
+    print(f"entrance_fee: {entrance_fee}, type: {type(entrance_fee)}")
+    print(f"interval: {interval}, type: {type(interval)}")
+    print(f"subscription_id: {subscription_id}, type: {type(subscription_id)}")
+    print(f"callback_gas_limit: {callback_gas_limit}, type: {type(callback_gas_limit)}")
 
     # Deploy the contract
     raffle_contract = raffle.deploy(
